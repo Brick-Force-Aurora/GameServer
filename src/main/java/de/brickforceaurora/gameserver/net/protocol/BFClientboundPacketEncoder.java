bbdc7c0d5f15156db7da34f679336c07f4e78723
Lauriichan <lauriichan@playuniverse.org>
2026-01-27 21:45:32 +0100

    Fix packet encoder & decoder (again)

--- src/main/java/de/brickforceaurora/gameserver/net/protocol/BFClientboundPacketEncoder.java
+++ src/main/java/de/brickforceaurora/gameserver/net/protocol/BFClientboundPacketEncoder.java
@@ -22,7 +22,7 @@ public final class BFClientboundPacketEncoder extends MessageToByteEncoder<IClie
 
     @Override
     protected void encode(ChannelHandlerContext ctx, IClientboundPacket msg, ByteBuf out) throws Exception {
-        ByteBuf bodyBuffer = Unpooled.directBuffer();
+        ByteBuf bodyBuffer = Unpooled.buffer();
         byte[] body;
         try {
             if (msg.requiresClientId()) {
